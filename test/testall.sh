ac3a97196af7b2d4a67b82d0cf82f90525ace686
giacomo-ascari <giacomolego@gmail.com>
2021-09-30 16:14:37 +0200

    refactor: better test format

--- test/testall.sh
+++ test/testall.sh
@@ -7,19 +7,21 @@ timestamp() {
 LOG_FOLDER="log/$(timestamp)"
 ../bin/compile.sh
 THRS=( 15 31 47 63 79 95 111 127 )
-for THR in "${THRS[@]}"
-do 
-  for EXAMPLE_PATH in ../examples/*
+for EXAMPLE_PATH in ../examples/*
+do
+  IMAGE=$(basename "$EXAMPLE_PATH")
+  IMAGE_FOLDER="$LOG_FOLDER/$IMAGE-DIR"
+  IMAGE_PATH="$IMAGE_FOLDER/$IMAGE"
+  mkdir -p $IMAGE_FOLDER
+  cp $EXAMPLE_PATH $IMAGE_PATH
+  for THR in "${THRS[@]}"
   do
-    IMAGE=$(basename "$EXAMPLE_PATH")
-    IMAGE_FOLDER="$LOG_FOLDER/$THR/$IMAGE-DIR"
-    IMAGE_PATH="$IMAGE_FOLDER/$IMAGE"
-    mkdir -p $IMAGE_FOLDER
-    cp $EXAMPLE_PATH $IMAGE_PATH
     ./test.sh $IMAGE_PATH $THR
     FS_RAW=$(stat --printf="%s" $IMAGE_PATH)
     FS_DCI=$(stat --printf="%s" $IMAGE_PATH.dci)
     CR=$(( FS_DCI * 100 / FS_RAW ))
-    touch "$IMAGE_FOLDER/$CR"
+    NEW_PATH="$IMAGE_PATH-T$THR-CR$CR"
+    mv "$IMAGE_PATH.dci" "$NEW_PATH.dci"
+    mv "$IMAGE_PATH.dci.png" "$NEW_PATH.dci.png"
   done
 done
